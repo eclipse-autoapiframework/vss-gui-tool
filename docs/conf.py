a7989737790b7c397f14056bc782b577987b8aa5
Pfleiderer Thomas FRD DISDS1 <thomaspfleiderer@zf.com>
2026-05-06 08:38:47 +0000

    fix documentation creation

--- docs/conf.py
+++ docs/conf.py
@@ -1,24 +1,23 @@
-..
-   # *******************************************************************************
-   # Copyright (c) 2026 Contributors to the Eclipse Foundation
-   #
-   # See the NOTICE file(s) distributed with this work for additional
-   # information regarding copyright ownership.
-   #
-   # This program and the accompanying materials are made available under the
-   # terms of the Apache License Version 2.0 which is available at
-   # https://www.apache.org/licenses/LICENSE-2.0
-   #
-   # SPDX-License-Identifier: Apache-2.0
-   #
-   # Contributors:
-   #   Thomas Pfleiderer - first documentation
-   # *******************************************************************************
+# *******************************************************************************
+# Copyright (c) 2026 Contributors to the Eclipse Foundation
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Apache License Version 2.0 which is available at
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+# Contributors:
+#   Thomas Pfleiderer - first documentation
+# *******************************************************************************
 
-   # Configuration file for the Sphinx documentation builder.
-   # This file only contains a selection of the most common options. For a full
-   # list see the documentation:
-   # https://www.sphinx-doc.org/en/master/usage/configuration.html     
+# Configuration file for the Sphinx documentation builder.
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html     
 
 
 # -- Project information -----------------------------------------------------
